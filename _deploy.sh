77ab4ae74a38597c2dc45dc6aee2adf2b098be4e
liao961120 <liaomovie2@gmail.com>
2018-04-06 17:55:02 +0800

    update

---
+++ _deploy.sh
@@ -0,0 +1,14 @@
+
+git config --global user.email "liaomovie2@gmail.com"
+git config --global user.name "liao961120"
+
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+cd book-output
+git rm -rf *
+cp -r ../docs/* ./
+git add --all *
+git commit -m"Update the book"
+git push -q origin gh-pages
