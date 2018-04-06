332eebbccf6d74124a28eeb2e8bb5bdd23df0afe
liao961120 <liaomovie2@gmail.com>
2018-04-06 18:30:14 +0800

    update

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@ git config --global user.email "liaomovie2@gmail.com"
 git config --global user.name "liao961120"
 
 # clone the repository to the book-output directory
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b gh-pages https://liao961120@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 git rm -rf *
 cp -r ../_book/* ./
